2739dba19809acd5e2fe8fa746154f6ec31dbfd1
bwj59 <bwj59@naver.com>
2022-02-18 08:16:32 +0900

    javascript pr 02-18

---
+++ 027/main.js
@@ -0,0 +1,9 @@
+let textarea = document.querySelector('.textarea');
+let string_num = document.querySelector('.string_num');
+
+textarea.addEventListener('keyup',onKeyUp);
+
+function onKeyUp(){
+    const inputText = textarea.value;
+    string_num.innerHTML = inputText.length
+}
