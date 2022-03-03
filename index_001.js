ec756cd5a05f802820494c8e2e550561a16523f0
bwj59 <bwj59@naver.com>
2022-03-03 14:37:56 +0900

    pr

--- index_001.js
+++ index_001.js
@@ -3,20 +3,22 @@ let array1 = new Array('baafe','awb','yagfc', 'Cafgag');
 let array2 = new Array({id:'bwj0509',pw:'1234'},{id:'bdj214',pw:'123463'});
 let array3 = new Array(2123132132,23, 523, 23, 5, 3, 7, 8,3,1, 1441, 1456, 523626262, 62233);
 
+gijun = document.querySelector('.gijun');
 
-function serchUser(targetId){
-    const userList = [
-        {id:1, name:'woojin'},
-        {id:2, name:'hanna'}
-    ]
+ch1 = document.querySelector('.ch1');
+ch2 = document.querySelector('.ch2');
 
-    const findUser = userList.find((user)=> user.id === targetId);
-    
-    if(findUser === undefined){
-        return null;
-    }
+container = document.querySelector('.container');
 
-    return findUser.name
-}
+let divElement = document.createElement('div');
+let anchorElement = document.createElement('a');
 
-console.log(serchUser(3))
+
+let makingdiv = document.querySelector('.btn1').addEventListener('click',()=>{
+    document.body.appendChild(divElement);
+    divElement.appendChild(anchorElement);
+    anchorElement.innerHTML = '성공!'
+})
+
+
+console.log()
