700d60d49e61e0cf2e4f481444c39fb38c8c200e
bwj59 <bwj59@naver.com>
2022-03-12 15:34:44 +0900

    first - last

--- 주제/begin-end, first-last/main.js
+++ 주제/begin-end, first-last/main.js
@@ -1,5 +1,5 @@
 /*
-begin - end
+1. begin - end
 */
 
 function reservationDate(beginDate, endDate){
@@ -8,3 +8,23 @@ function reservationDate(beginDate, endDate){
 
 reservationDate('YYYY-MM-DD','YYYY-MM-DD')
 // 함수명을 정확하게 하면 매개변수에 어떠한 값이 들어가고 어떻게 동작할 지 예측이 가능하다.
+
+
+
+
+
+/*
+2. first - last
+*/
+const Student = ['1번학생', '2번학생', '3번학생', '4번학생']
+function getStudents(first, last){
+    //...somecode
+}
+
+getStudents('1번학생','4번학생');
+
+
+
+/*
+3.prefix - suffix
+*/
